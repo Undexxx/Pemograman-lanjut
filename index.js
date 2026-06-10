c96188ea3f2c1ae6329fb4cf940aeb95db4cdf60
Undexxx <udeaxxx@gmail.com>
2026-06-10 10:18:12 +0700

    changes

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import db from "./config/db.config.js";
+import bukus from "./routes/buku.routes.js"
+import mahasiswas from "./routes/mahasiswa.routes.js"
+import prodis from "./routes/prodi.routes.js"
+import pinjams from "./routes/pinjam.routes.js"
+import detail_pinjams from "./routes/detail_pinjam.routes.js";
+import cors from "cors";
+
+const app=express();
+try {
+    await db.authenticate();
+    console.log("database ok");
+
+    await db.sync();
+    console.log ("database tersingkronisasi");
+    
+} catch (error) {
+    console.log("belum konek",error);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/buku',bukus);
+app.use('/api/mahasiswa',mahasiswas);
+app.use('/api/prodi',prodis);
+app.use('/api/pinjam',pinjams);
+app.use('/api/detail_pinjam',detail_pinjams);
+
+app.listen(5000);
